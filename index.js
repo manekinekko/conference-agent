2cb33fa33e927325107c574ac8f824b237e55934
Wassim CHEGHAM <github@wassimchegham.com>
2017-05-08 15:52:59 +0200

    ✌️

--- index.js
+++ index.js
@@ -5,38 +5,56 @@ const util = require('util');
 
 const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
 const fetch = require('node-fetch');
-const async = require('asyncawait/async');
-const await = require('asyncawait/await');
 
 // intents
-const LIST_TOPICS_INTENT = 'list-topics';
+const LIST_TOPICS_INTENT = 'topics.list';
+const WELCOME_INTENT = 'input.welcome';
+const SPEAKERS_COUNT_INTENT = 'speakers.count';
 
-const _fetchSchedule = async(() => {
+//helpers
+const _debug = (name, what) => {
+    fs.writeFileSync(`./logs/${name}-headers.json`, JSON.stringify(what.headers, null, 2));
+    fs.writeFileSync(`./logs/${name}-body.json`, JSON.stringify(what.body, null, 2));
+}
+
+const _fetchSchedule = () => {
     let schedules = ['thursday', 'friday'].map(day => {
-        const rawResponse = await (fetch(`http://cfp.devoxx.co.uk/api/conferences/DV17/schedules/${day}`));
-        const response = await (rawResponse.text());
-        return JSON.parse(response);
-    }).map(d => d.slots);
+        return fetch(`http://cfp.devoxx.co.uk/api/conferences/DV17/schedules/${day}`)
+            .then(rawResponse => rawResponse.text())
+            .then(textResponse => JSON.parse(textResponse));
+    });
 
-    return [].concat(
-        ...schedules
-    );
-});
-
-const filter = (schedule, predicat) => {
-    return predicat(schedule);
+    return Promise.all(schedules)
+        .then(data => data.map(d => d.slots))
+        .then(slots => [].concat(...slots));
 };
+
+const _filter = (schedule, predicat, comparator) => {
+    return predicat(schedule, comparator);
+};
+
+// Predicates
 
 const byTalkName = (schedule) => {
     return schedule
-        .filter(s => s.talk)
-        .map(s => s.talk.title.trim());
+        .filter(slot => slot.talk)
+        .map(slot => slot.talk.title.trim());
+
+};
+
+const byTalkTrack = (schedule) => {
+    schedule = schedule
+        .filter(slot => slot.talk)
+        .map(slot => slot.talk.track.trim().replace('&amp;', 'and'));
+    return [
+        ...new Set(schedule)
+    ];
 };
 
 const byTalkType = (schedule) => {
     schedule = schedule
-        .filter(s => s.talk)
-        .map(s => s.talk.talkType.trim());
+        .filter(slot => slot.talk)
+        .map(slot => slot.talk.talkType.trim());
     return [
         ...new Set(schedule)
     ];
@@ -44,40 +62,106 @@ const byTalkType = (schedule) => {
 
 const byRoom = (schedule) => {
     schedule = schedule
-        .filter(s => s.talk)
-        .map(s => s.roomName.trim());
+        .filter(slot => slot.talk)
+        .map(slot => slot.roomName.trim());
     return [
         ...new Set(schedule)
     ];
 };
 
 const bySpeaker = (schedule) => {
-    return [
-        ...new Set(
-            schedule
-            .filter(s => s.talk)
-            .map(s => s.talk.speakers.map(s => s.name.trim()).pop())
-            .sort()
-        )
-    ];
+    const speakers = schedule
+        .filter(slot => slot.talk)
+        .map(slot => slot.talk.speakers.map(speaker => speaker.name.trim()).pop())
+        .sort();
+    return new Set(speakers);
 };
 
+const byTopic = (schedule, topic) => {
+    const talks = schedule
+        .filter(slot => {
+            return slot.talk && slot.talk.track.toLowerCase().indexOf(topic.toLowerCase()) !== -1;
+        })
+    if (talks.length > 0) {
+        return talks.map(slot => slot.talk);
+    } else {
+        return [];
+    }
+};
+
+
+// Intents
+
 const listTopicsIntent = (assistant) => {
-    // const schedule = await (_fetchSchedule());
-    // const topics = filter(schedule, byTalkType);
-    // console.log(topics);
-    assistant.ask(`The topics covered are: . 
-                    What do you want to learn about?'`);
+    _fetchSchedule()
+        .then(schedule => _filter(schedule, byTalkTrack))
+        .then(topics => {
+            assistant.ask(`The covered topics are: ${topics.join(', ')}. 
+                            What do you want to learn about?'`);
+        })
+        .catch(error => assistant.ask(error.toString()));
+};
+
+const speakersCountIntent = (assistant) => {
+    _fetchSchedule()
+        .then(schedule => _filter(schedule, bySpeaker))
+        .then(speakers => {
+            assistant.ask(`I found ${speakers.length} speakers.`);
+        })
+        .catch(error => assistant.ask(error.toString()));
+};
+
+const talksByTopicsIntent = (assistant) => {
+    let topic = assistant.getArgument('topic-name');
+    let isNext = assistant.getArgument('date-period');
+    topic = topic.toLowerCase().replace('and ', '& ');
+
+    if (topic === '') {
+        assistant.ask(`Sorry, I didn't get what topic you were interested in. 
+                       Is there another topic you'd like to hear about?`);
+    } else {
+        _fetchSchedule()
+            .then(schedule => _filter(schedule, byTopic, topic))
+            .then(talks => {
+                const totalSessions = talks.length;
+                if (totalSessions === 0) {
+                    assistant.ask(`Sorry, I couldn't find any session about ${topic}.
+                                   Is there another topic you'd be interested in?`);
+                } else {
+                    assistant.setContext('find-next-by-topic', 3, {
+                        sessionIndex: 0,
+                        talkIds: talks.map(talk => talk.id),
+                        totalSessions
+                    });
+                    assistant.ask(`The next session about ${topic} is called ${talks[0].title}.
+                                   Would you like to hear more about it or hear about the next session?`);
+                }
+            })
+            .catch(error => {
+                console.log(`Error while fetching sessions: ${error}`);
+
+                assistant.tell(`I wasn't able to reach the Devoxx REST API 
+                                to list the available sessions about ${topic}.`);
+            });
+    }
+};
+
+const welcomeIntent = (assistant) => {
+    assistant.ask(`Welcome to your Devoxx bot! I am Groot!`);
+    // I can help you find a talk, list all topics, know more about a workshop or a speaker.
+    // What would you like to know?`);
 };
 
 exports.devoxx = (request, response) => {
-    fs.writeFileSync('./request.json', util.inspect(request, false, 5));
-    let assistant = new ApiAiAssistant({ request, response });
-    let actionMap = new Map();
-    actionMap.set(LIST_TOPICS_INTENT, (assistant) => {
-        assistant.ask(`The topics covered are: . 
-                    What do you want to learn about?'`);
-    });
+    _debug('request', request);
+
+    const assistant = new ApiAiAssistant({ request, response });
+    const actionMap = new Map();
+    actionMap.set('input.welcome', welcomeIntent);
+    actionMap.set('speakers.count', speakersCountIntent);
+    actionMap.set('topics.list', listTopicsIntent);
+    actionMap.set('talks.by.topic', talksByTopicsIntent);
     assistant.handleRequest(actionMap);
-    fs.writeFileSync('./response.json', util.inspect(request, false, 5));
+
+    _debug('response', response);
 };
